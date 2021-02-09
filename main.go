5128568742739795a1b9d9c4ef0525e7605e3693
Frank Rosner <frank.rosner@datastax.com>
2021-02-09 10:20:14 +0100

    format

--- main.go
+++ main.go
@@ -40,8 +40,8 @@ func (socket *Socket) Close() error {
 var socket *Socket
 
 func main() {
-    ip := "127.0.0.1"
-    port := 8080
+	ip := "127.0.0.1"
+	port := 8080
 
 	/*
    	Ensure we close the socket on shutdown
@@ -59,154 +59,154 @@ func main() {
 		os.Exit(0)
 	}()
 
-    /*
-    Create a Socket.
+	/*
+	Create a Socket.
 
-     - AF_INET = ARPA Internet protocols (IP)
-     - SOCK_STREAM = sequenced, reliable, two-way connection based byte streams
+	 - AF_INET = ARPA Internet protocols (IP)
+	 - SOCK_STREAM = sequenced, reliable, two-way connection based byte streams
 
-     See https://developer.apple.com/library/archive/documentation/System/Conceptual/ManPages_iPhoneOS/man2/socket.2.html
-    */
-    socketFileDescriptor, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, 0)
-    socket = &Socket{FileDescriptor: socketFileDescriptor}
-    if err != nil {
-        log.Println("Failed to create Socket:", err)
-        os.Exit(1)
-    }
+	 See https://developer.apple.com/library/archive/documentation/System/Conceptual/ManPages_iPhoneOS/man2/socket.2.html
+	*/
+	socketFileDescriptor, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, 0)
+	socket = &Socket{FileDescriptor: socketFileDescriptor}
+	if err != nil {
+		log.Println("Failed to create Socket:", err)
+		os.Exit(1)
+	}
 	log.Print("Created new socket ", socket)
 
-    /*
-    Bind the Socket to a port
-    */
-    socketAddress := &syscall.SockaddrInet4{Port: port}
-    copy(socketAddress.Addr[:], net.ParseIP(ip))
-    if err = syscall.Bind(socket.FileDescriptor, socketAddress); err != nil {
-        log.Println("Failed to bind socket:", err)
-        os.Exit(1)
-    }
+	/*
+	Bind the Socket to a port
+	*/
+	socketAddress := &syscall.SockaddrInet4{Port: port}
+	copy(socketAddress.Addr[:], net.ParseIP(ip))
+	if err = syscall.Bind(socket.FileDescriptor, socketAddress); err != nil {
+		log.Println("Failed to bind socket:", err)
+		os.Exit(1)
+	}
 	log.Print("Bound socket ", socket, " on ", ip, ":", port)
 
-    /*
-    Listen for incoming connections.
+	/*
+	Listen for incoming connections.
 
-    See https://developer.apple.com/library/archive/documentation/System/Conceptual/ManPages_iPhoneOS/man2/listen.2.html
-    */
-    if err = syscall.Listen(socket.FileDescriptor, syscall.SOMAXCONN); err != nil {
-        log.Println("Failed to listen on Socket:", err)
-        os.Exit(1)
-    }
+	See https://developer.apple.com/library/archive/documentation/System/Conceptual/ManPages_iPhoneOS/man2/listen.2.html
+	*/
+	if err = syscall.Listen(socket.FileDescriptor, syscall.SOMAXCONN); err != nil {
+		log.Println("Failed to listen on Socket:", err)
+		os.Exit(1)
+	}
 	log.Print("Listening on socket ", socket)
 
-    /*
-    Create new new kernel event queue
+	/*
+	Create new new kernel event queue
 
-    See https://www.freebsd.org/cgi/man.cgi?query=kqueue&sektion=2
-    */
-    kQueue, err := syscall.Kqueue()
-    if err != nil {
-        log.Println("Failed to create kernel event queue:", err)
-        os.Exit(1)
-    }
+	See https://www.freebsd.org/cgi/man.cgi?query=kqueue&sektion=2
+	*/
+	kQueue, err := syscall.Kqueue()
+	if err != nil {
+		log.Println("Failed to create kernel event queue:", err)
+		os.Exit(1)
+	}
 	log.Print("Created kqueue ", kQueue)
 
-    /*
-    Specify event we want to monitor.
+	/*
+	Specify event we want to monitor.
 
-    - EVFILT_READ -> receive only events when there is data to read on the Socket
-    - EV_ADD | EV_ENABLE -> add event and enable it
+	- EVFILT_READ -> receive only events when there is data to read on the Socket
+	- EV_ADD | EV_ENABLE -> add event and enable it
 
-    See https://www.freebsd.org/cgi/man.cgi?query=kqueue&sektion=2
-    */
-    changeEvent := syscall.Kevent_t{
-        Ident:  uint64(socket.FileDescriptor),
-        Filter: syscall.EVFILT_READ,
-        Flags:  syscall.EV_ADD | syscall.EV_ENABLE,
-        Fflags: 0,
-        Data:   0,
-        Udata:  nil,
-    }
+	See https://www.freebsd.org/cgi/man.cgi?query=kqueue&sektion=2
+	*/
+	changeEvent := syscall.Kevent_t{
+		Ident:  uint64(socket.FileDescriptor),
+		Filter: syscall.EVFILT_READ,
+		Flags:  syscall.EV_ADD | syscall.EV_ENABLE,
+		Fflags: 0,
+		Data:   0,
+		Udata:  nil,
+	}
 
-    /*
-    The kevent() system call is used to register events with the queue, and return any pending events to the user.
-    First, we register the change event with the queue, leaving the third argument empty.
+	/*
+	The kevent() system call is used to register events with the queue, and return any pending events to the user.
+	First, we register the change event with the queue, leaving the third argument empty.
 
-    See https://www.freebsd.org/cgi/man.cgi?query=kqueue&sektion=2
-    */
-    changeEventRegistered, err := syscall.Kevent(kQueue, []syscall.Kevent_t{changeEvent}, nil, nil)
-    if err != nil || changeEventRegistered == -1 {
-        log.Print("Failed to register changeEvent:", err)
-        os.Exit(1)
-    }
+	See https://www.freebsd.org/cgi/man.cgi?query=kqueue&sektion=2
+	*/
+	changeEventRegistered, err := syscall.Kevent(kQueue, []syscall.Kevent_t{changeEvent}, nil, nil)
+	if err != nil || changeEventRegistered == -1 {
+		log.Print("Failed to register changeEvent:", err)
+		os.Exit(1)
+	}
 
-    log.Println("Server started. Waiting for incoming connections. ^C to exit.")
-    /*
-    Event loop, checking the kernel queue for new events and executing handlers.
-    */
-    for {
-        /*
-        Then, we query the queue for pending events, leaving the second argument empty.
-        */
-        newEvents := make([]syscall.Kevent_t, 10)
-        numNewEvents, err := syscall.Kevent(kQueue, nil, newEvents, nil)
-        if err != nil {
-            log.Println("Failed to read new events:", err)
-            os.Exit(1)
-        }
+	log.Println("Server started. Waiting for incoming connections. ^C to exit.")
+	/*
+	Event loop, checking the kernel queue for new events and executing handlers.
+	*/
+	for {
+		/*
+		Then, we query the queue for pending events, leaving the second argument empty.
+		*/
+		newEvents := make([]syscall.Kevent_t, 10)
+		numNewEvents, err := syscall.Kevent(kQueue, nil, newEvents, nil)
+		if err != nil {
+			log.Println("Failed to read new events:", err)
+			os.Exit(1)
+		}
 
-        for i := 0; i < numNewEvents; i++ {
-            currentEvent := newEvents[i]
-            eventFileDescriptor := int(currentEvent.Ident)
+		for i := 0; i < numNewEvents; i++ {
+			currentEvent := newEvents[i]
+			eventFileDescriptor := int(currentEvent.Ident)
 
-            if currentEvent.Flags & syscall.EV_EOF != 0 {
-                /*
-                Handle client closing the connection. Closing the event file descriptor removes it from the queue.
-                */
-                log.Println("Client disconnected.")
-                syscall.Close(eventFileDescriptor)
-            } else if eventFileDescriptor == socket.FileDescriptor {
-                /*
-                Accept incoming connection.
-                */
-                socketConnection, _, err := syscall.Accept(eventFileDescriptor)
-                if err != nil {
-                    log.Println("Failed to create Socket for connecting to client:", err)
-                    continue
-                }
+			if currentEvent.Flags&syscall.EV_EOF != 0 {
+				/*
+				Handle client closing the connection. Closing the event file descriptor removes it from the queue.
+				*/
+				log.Println("Client disconnected.")
+				syscall.Close(eventFileDescriptor)
+			} else if eventFileDescriptor == socket.FileDescriptor {
+				/*
+				Accept incoming connection.
+				*/
+				socketConnection, _, err := syscall.Accept(eventFileDescriptor)
+				if err != nil {
+					log.Println("Failed to create Socket for connecting to client:", err)
+					continue
+				}
 				log.Print("Accepted new connection ", socketConnection, " from ", eventFileDescriptor)
 
-                /*
-                Watch for data coming in through the new connection.
-                */
-                socketEvent := syscall.Kevent_t{
-                    Ident:  uint64(socketConnection),
-                    Filter: syscall.EVFILT_READ,
-                    Flags:  syscall.EV_ADD,
-                    Fflags: 0,
-                    Data:   0,
-                    Udata:  nil,
-                }
-                socketEventRegistered, err := syscall.Kevent(kQueue, []syscall.Kevent_t{socketEvent}, nil, nil)
-                if err != nil || socketEventRegistered == -1 {
-                    log.Print("Failed to register Socket event:", err)
-                    continue
-                }
-            } else if currentEvent.Filter & syscall.EVFILT_READ != 0 {
-            	/*
-            	Echo incoming data until empty line is received.
-            	*/
-                clientSocket := Socket{FileDescriptor: int(eventFileDescriptor)}
+				/*
+				Watch for data coming in through the new connection.
+				*/
+				socketEvent := syscall.Kevent_t{
+					Ident:  uint64(socketConnection),
+					Filter: syscall.EVFILT_READ,
+					Flags:  syscall.EV_ADD,
+					Fflags: 0,
+					Data:   0,
+					Udata:  nil,
+				}
+				socketEventRegistered, err := syscall.Kevent(kQueue, []syscall.Kevent_t{socketEvent}, nil, nil)
+				if err != nil || socketEventRegistered == -1 {
+					log.Print("Failed to register Socket event:", err)
+					continue
+				}
+			} else if currentEvent.Filter&syscall.EVFILT_READ != 0 {
+				/*
+				Echo incoming data until empty line is received.
+				*/
+				clientSocket := Socket{FileDescriptor: int(eventFileDescriptor)}
 				reader := bufio.NewReader(clientSocket)
 				for {
 					line, err := reader.ReadString('\n')
 					if err != nil || strings.TrimSpace(line) == "" {
 						break
 					}
-					log.Print("Read on ", eventFileDescriptor ,": ", line)
+					log.Print("Read on ", eventFileDescriptor, ": ", line)
 					clientSocket.Write([]byte(line))
 				}
 				clientSocket.Close()
-            }
-            // Ignore any other events
-        }
-    }
+			}
+			// Ignore any other events
+		}
+	}
 }
