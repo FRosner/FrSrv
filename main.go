6eaa0df8e12dd672b6a429ca8ca4cfe0c47455ec
Frank Rosner <frank.rosner@datastax.com>
2021-02-11 20:33:44 +0100

    remove some obsolete comments

--- main.go
+++ main.go
@@ -10,26 +10,12 @@ import (
 )
 
 func main() {
-	/*
-	Create a Socket.
-
-	- AF_INET = ARPA Internet protocols (IP)
-	- SOCK_STREAM = sequenced, reliable, two-way connection based byte streams
-
-	See https://developer.apple.com/library/archive/documentation/System/Conceptual/ManPages_iPhoneOS/man2/socket.2.html
-	*/
-
 	s, err := socket.Listen("127.0.0.1", 8080)
 	if err != nil {
 		log.Println("Failed to create Socket:", err)
 		os.Exit(1)
 	}
 
-	/*
-	Create new new kernel event queue
-
-	See https://www.freebsd.org/cgi/man.cgi?query=kqueue&sektion=2
-	*/
 	eventLoop, err := kqueue.NewEventLoop(s)
 	if err != nil {
 		log.Println("Failed to create kqueue:", err)
