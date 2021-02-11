4dd4f4afdade811ef41c9a62111fa5fda85778b4
Frank Rosner <frank.rosner@datastax.com>
2021-02-11 20:21:07 +0100

    event loop class

--- kqueue/kqueue.go
+++ kqueue/kqueue.go
@@ -7,11 +7,14 @@ import (
 	"syscall"
 )
 
-type Kqueue struct {
-	FileDescriptor int
+type EventLoop struct {
+	KqueueFileDescriptor int
+	SocketFileDescriptor int
 }
 
-func FromSocket(s *socket.Socket) (*Kqueue, error) {
+type Handler = func(s *socket.Socket)
+
+func NewEventLoop(s *socket.Socket) (*EventLoop, error) {
 	/*
 	Create new new kernel event queue
 
@@ -51,5 +54,70 @@ func FromSocket(s *socket.Socket) (*Kqueue, error) {
 		return nil, fmt.Errorf("failed to register change event (%v)", err)
 	}
 
-	return &Kqueue{FileDescriptor: kQueue}, nil
+	return &EventLoop{KqueueFileDescriptor: kQueue, SocketFileDescriptor: s.FileDescriptor}, nil
+}
+
+func (eventLoop *EventLoop) Handle(handler Handler) {
+	/*
+	Event loop, checking the kernel queue for new events and executing handlers.
+	*/
+	for {
+		/*
+		Then, we query the queue for pending events, leaving the second argument empty.
+		*/
+		log.Println("Polling for new events...")
+		newEvents := make([]syscall.Kevent_t, 10)
+		numNewEvents, err := syscall.Kevent(eventLoop.KqueueFileDescriptor, nil, newEvents, nil)
+		if err != nil {
+			/*
+			We sometimes get syscall.Errno == 0x4 (EINTR) but that's ok it seems. Just keep polling.
+			See https://reviews.llvm.org/D42206
+			*/
+			continue
+		}
+
+		for i := 0; i < numNewEvents; i++ {
+			currentEvent := newEvents[i]
+			eventFileDescriptor := int(currentEvent.Ident)
+
+			if currentEvent.Flags&syscall.EV_EOF != 0 {
+				/*
+				Handle client closing the connection. Closing the event file descriptor removes it from the queue.
+				*/
+				log.Println("Client disconnected.")
+				syscall.Close(eventFileDescriptor)
+			} else if eventFileDescriptor == eventLoop.SocketFileDescriptor {
+				/*
+				Accept incoming connection.
+				*/
+				socketConnection, _, err := syscall.Accept(eventFileDescriptor)
+				if err != nil {
+					log.Println("Failed to create Socket for connecting to client:", err)
+					continue
+				}
+				log.Print("Accepted new connection ", socketConnection, " from ", eventFileDescriptor)
+
+				/*
+				Watch for data coming in through the new connection.
+				*/
+				socketEvent := syscall.Kevent_t{
+					Ident:  uint64(socketConnection),
+					Filter: syscall.EVFILT_READ,
+					Flags:  syscall.EV_ADD,
+					Fflags: 0,
+					Data:   0,
+					Udata:  nil,
+				}
+				socketEventRegistered, err := syscall.Kevent(eventLoop.KqueueFileDescriptor, []syscall.Kevent_t{socketEvent}, nil, nil)
+				if err != nil || socketEventRegistered == -1 {
+					log.Print("Failed to register Socket event:", err)
+					continue
+				}
+			} else if currentEvent.Filter&syscall.EVFILT_READ != 0 {
+				clientSocket := socket.FromFileDescriptor(int(eventFileDescriptor))
+				handler(clientSocket)
+			}
+			// Ignore any other events
+		}
+	}
 }
--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"log"
 	"os"
 	"strings"
-	"syscall"
 )
 
 func main() {
@@ -31,85 +30,23 @@ func main() {
 
 	See https://www.freebsd.org/cgi/man.cgi?query=kqueue&sektion=2
 	*/
-	kQueue, err := kqueue.FromSocket(s)
+	eventLoop, err := kqueue.NewEventLoop(s)
 	if err != nil {
 		log.Println("Failed to create kqueue:", err)
 		os.Exit(1)
 	}
 
 	log.Println("Server started. Waiting for incoming connections. ^C to exit.")
-	/*
-	Event loop, checking the kernel queue for new events and executing handlers.
-	*/
-	for {
-		/*
-		Then, we query the queue for pending events, leaving the second argument empty.
-		*/
-		log.Println("Polling for new events...")
-		newEvents := make([]syscall.Kevent_t, 10)
-		numNewEvents, err := syscall.Kevent(kQueue.FileDescriptor, nil, newEvents, nil)
-		if err != nil {
-			/*
-			We sometimes get syscall.Errno == 0x4 (EINTR) but that's ok it seems. Just keep polling.
-			See https://reviews.llvm.org/D42206
-			*/
-			continue
-		}
-
-		for i := 0; i < numNewEvents; i++ {
-			currentEvent := newEvents[i]
-			eventFileDescriptor := int(currentEvent.Ident)
-
-			if currentEvent.Flags&syscall.EV_EOF != 0 {
-				/*
-				Handle client closing the connection. Closing the event file descriptor removes it from the queue.
-				*/
-				log.Println("Client disconnected.")
-				syscall.Close(eventFileDescriptor)
-			} else if eventFileDescriptor == s.FileDescriptor {
-				/*
-				Accept incoming connection.
-				*/
-				socketConnection, _, err := syscall.Accept(eventFileDescriptor)
-				if err != nil {
-					log.Println("Failed to create Socket for connecting to client:", err)
-					continue
-				}
-				log.Print("Accepted new connection ", socketConnection, " from ", eventFileDescriptor)
-
-				/*
-				Watch for data coming in through the new connection.
-				*/
-				socketEvent := syscall.Kevent_t{
-					Ident:  uint64(socketConnection),
-					Filter: syscall.EVFILT_READ,
-					Flags:  syscall.EV_ADD,
-					Fflags: 0,
-					Data:   0,
-					Udata:  nil,
-				}
-				socketEventRegistered, err := syscall.Kevent(kQueue.FileDescriptor, []syscall.Kevent_t{socketEvent}, nil, nil)
-				if err != nil || socketEventRegistered == -1 {
-					log.Print("Failed to register Socket event:", err)
-					continue
-				}
-			} else if currentEvent.Filter&syscall.EVFILT_READ != 0 {
-				/*
-				Echo incoming data until empty line is received.
-				*/
-				clientSocket := socket.FromFileDescriptor(int(eventFileDescriptor))
-				reader := bufio.NewReader(clientSocket)
-				for {
-					line, err := reader.ReadString('\n')
-					if err != nil || strings.TrimSpace(line) == "" {
-						break
-					}
-					log.Print("Read on ", eventFileDescriptor, ": ", line)
-					clientSocket.Write([]byte(line))
-				}
-				clientSocket.Close()
+	eventLoop.Handle(func(s *socket.Socket) {
+		reader := bufio.NewReader(s)
+		for {
+			line, err := reader.ReadString('\n')
+			if err != nil || strings.TrimSpace(line) == "" {
+				break
 			}
-			// Ignore any other events
+			log.Print("Read on ", s, ": ", line)
+			s.Write([]byte(line))
 		}
-	}
+		s.Close()
+	})
 }
